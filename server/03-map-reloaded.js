b7f08da587037e3e6dd80f8ba709249eb57069f1
Enrique <octepc.97@gmail.com>
2022-04-18 20:12:16 -0500

    como funciona .map

--- server/03-map-reloaded.js
+++ server/03-map-reloaded.js
@@ -20,21 +20,27 @@ const orders = [
     delivered: true,
   },
 ];
-console.log("original", orders);
-const rta = orders.map((item) => item.total);
-console.log("rta", rta);
 
-// const rta2 = orders.map((item) => {
-//   item.tax = 0.19;
-//   return item;
+console.log("original", orders);
+
+const rpta = orders.map((order) => {
+  return order.total;
+});
+
+console.log("rpta", rpta);
+
+// const rpta2 = orders.map((order) => {
+//   order.tax = 0.19;
+//   return orders;
 // });
-// console.log("rta2", rta2);
+// console.log("rpta2", rpta2);
 // console.log("original", orders);
-const rta3 = orders.map((item) => {
+
+const rpta3 = orders.map((order) => {
   return {
-    ...item,
+    ...order,
     tax: 0.19,
   };
 });
-console.log("rta3", rta3);
+console.log("rpta3", rpta3);
 console.log("original", orders);
